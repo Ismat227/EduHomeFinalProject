444b865d4bc3725dd77b1e90da245a3b3a5782b0
Ismat227 <ismatad@github.com>
2021-09-25 00:34:49 +0400

    CompletedProject

---
+++ BackendProject/Areas/admin/ViewModels/UserViewMoel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendProject.Areas.admin.ViewModels
+{
+    public class UserViewMoel
+    {
+        public string Fullname { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public bool IsDeletedet { get; set; }
+    }
+}
---
+++ BackendProject/Helpers/CustomErrorLanguage.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendProject.Helpers
+{
+    public class CustomErrorLanguage:IdentityErrorDescriber
+    {
+        public override IdentityError PasswordRequiresDigit()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresDigit),
+                Description="Parolda reqem olmalidi"
+            };
+        }
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError
+            {
+                Code=nameof(PasswordTooShort),
+                Description= $"Parolun uzunlugu {length}-bu qeder olmalidi"
+            };
+        }
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresLower),
+                Description = "Parolda kiçik hərflər də daxil elə"
+            };
+        }
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError
+            {
+                Code=nameof(PasswordRequiresUpper),
+                Description= "Parola böyük hərflər də daxil elə"
+            };
+        }
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError
+            {
+                Code=nameof(PasswordRequiresNonAlphanumeric),
+                Description= "Parola simvol daxil et"
+            };
+        }
+        public override IdentityError DuplicateEmail(string email)
+        {
+            return new IdentityError
+            {
+                Code=nameof(DuplicateEmail),
+                Description="Bu email istiade olunub"
+            };
+        }
+    }
+}
